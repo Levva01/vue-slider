baa212c7fa06e3726875e3987f37dd283ed37b79
Levva01 <vellanifederico13@gmail.com>
2022-05-20 15:05:23 +0200

    prima parte completata

--- js/main.js
+++ js/main.js
@@ -25,3 +25,59 @@ const images = [
         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     },
 ];
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        currentIndex: 0,
+        images: [
+            {
+                src: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                src: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum',
+            },
+            {
+                src: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                src: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                src: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ]
+    },
+    methods: {
+
+        prev(){
+            if(this.currentIndex == 0){
+                this.currentIndex = this.images.lenght;
+            } else {
+                this.currentIndex--;
+            }
+        },
+
+        next(){
+            if(this.currentIndex == 0){
+                this.currentIndex = this.images.lenght;
+            } else {
+                this.currentIndex++;
+            }
+        },
+
+        selectImg(index) {
+            this.currentIndex = index;
+        }
+    }
+});
